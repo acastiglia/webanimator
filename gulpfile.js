9ea67cf5bc904def9e9cad146beb570c1e70d46d
Anthony Castiglia <castiglia.anthony@gmail.com>
2015-04-09 12:08:34 -0400

    Added index.html.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+
 var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
@@ -6,13 +7,13 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
 gulp.task('lint', function() {
-  return gulp.src('js/*.js')
+  return gulp.src('src/js/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('js/*.js')
+  return gulp.src('src/js/*.js')
     .pipe(concat('all.js'))
     .pipe(gulp.dest('dist'))
     .pipe(rename('all.min.js'))
--- src/animations/animation1.js
+++ src/animations/animation1.js
@@ -3,11 +3,13 @@ var model = new Model();
 
 model.advance = function(timestamp) {
   this.x = 250 + 250 * Math.sin(0.001 * timestamp);
+  this.y = 250 + 250 * Math.cos(0.003 * timestamp);
 };
 
 var renderer = new Renderer();
 renderer.render = function() {
   document.getElementById('circ').setAttribute('cx', model.x);
+  document.getElementById('circ').setAttribute('cy', model.y);
 };
 
 var controller = new AnimationController();
