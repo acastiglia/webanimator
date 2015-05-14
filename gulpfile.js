543756af5442a7a44c4fe62c4d752346400b7711
Anthony Castiglia <castiglia.anthony@gmail.com>
2015-05-13 23:05:45 -0400

    Add gl-matrix bower dependency

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var bowerMain = require('bower-main');
 
 var JS_SRC = 'src/js';
+var BOWER_JS_FILES = bowerMain('js','min.js');
 
 gulp.task('lint', function() {
   return gulp.src('JS_SRC/**/*.js')
@@ -14,14 +16,20 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter('default'));
 });
 
+gulp.task('vendorscripts', function() {
+  return gulp.src(BOWER_JS_FILES.normal)
+    .pipe(concat('vendor-scripts.js'))
+    .pipe(gulp.dest('dev'));
+});
+
 gulp.task('scripts', function() {
   return gulp.src(['src/js/util/*.js', 'src/js/**/*.js'])
-    .pipe(concat('all.js'))
+    .pipe(concat('webanimator.js'))
     .pipe(gulp.dest('dist'))
-    .pipe(rename('all.min.js'))
+    .pipe(rename('webanimator.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['lint', 'scripts']);
+gulp.task('default', ['lint', 'vendorscripts', 'scripts']);
 
